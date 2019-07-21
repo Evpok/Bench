f23f151abedb6ff45e1de1e935b659b2c5a38d21
Evpok Padding <evpok.padding@gmail.com>
2019-07-21 19:49:30 +0200

    add run instructions and config

---
+++ 01_Mandelbrot/Mandelbrot_python/README.md
@@ -0,0 +1,5 @@
+## How to run
+
+1. (optional) create and activate a virtualenv `python3.7 -m venv .virtenv && source .virtenv/bin/activate`
+2. install the requirements and build the extensions `pip install -e .`
+3. run the benchmark `python Mandelbrot.py`
--- 01_Mandelbrot/Mandelbrot_python/setup.py
+++ 01_Mandelbrot/Mandelbrot_python/setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 extensions = [
     Extension(
-        "CMandelbrot",
-        ["CMandelbrot.pyx"],
+        name="CMandelbrot",
+        sources=["CMandelbrot.pyx"],
         extra_compile_args=["-O3", "-fopenmp"],
         extra_link_args=["-fopenmp"],
     )
 ]
-setup(ext_modules=cythonize(extensions, language_level=3))
+
+setup(
+    install_requires=["numba", "numpy", "cython"],
+    ext_modules=cythonize(extensions, language_level=3),
+)
